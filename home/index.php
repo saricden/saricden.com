8d2dbd12d22c1cb58d875d4941b4d11460b2bf6a
Kirk Morris <kirk@saricden.com>
2014-12-01 21:27:49 -0800

    Adds root redirect & initial layout gurl.

---
+++ home/index.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Kirk M. // saricDen</title>
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+                color: inherit;
+                font-family: sans-serif;
+                font-size: 0.92rem;
+            }
+            html, body {
+                height: 100%;
+                background-color: #5C3A0A;
+            }
+            nav {
+                width: 200px;
+                height: 100%;
+                float: left;
+                color: #FFF;
+                background-color: #7A7;
+                letter-spacing: 1px;
+            }
+            main {
+                min-height: 350px;
+                padding-top: 30px;
+                background-color: #FFF;
+            }
+            h1, nav h2 {
+                font-weight: normal;
+            }
+            h1 {
+                padding: 15px 0;
+                text-align: center;
+                text-transform: uppercase;
+                font-size: 40px;
+            }
+            nav h2 {
+                color: #FFF;
+                background-color: #585;
+            }
+            nav h2 {
+                padding: 10px;
+            }
+            nav ul li a {
+                display: block;
+                padding: 15px 0;
+                padding-right: 9px;
+                border-top: solid 1px #585;
+                border-right: solid 1px #585;
+                text-align: right;
+                text-decoration: none;
+            }
+            nav ul li:last-child a {
+                border-bottom: solid 1px #585;
+            }
+            nav ul li.active a {
+                border-right-color: #FFF;
+                color: #585;
+                background-color: #FFF;
+            }
+        </style>
+    </head>
+    <body>
+        <nav>
+            <h2><strong>saricDen</strong> by Kirk</h2>
+            <ul>
+                <li class="active"><a href="../home">Home &raquo;</a></li>
+                <li><a href="../about">About me &raquo;</a></li>
+                <li><a href="../projects">Projects <strong>(16)</strong> &raquo;</a></li>
+                <li><a href="../guestbook">Guestbook <strong>(69)</strong> &raquo;</a></li>
+                <li><a href="../contact">Contact &raquo;</a></li>
+            </ul>
+        </nav>
+        <main>
+            <h1>Kirk M. // saricDen</h1>
+            
+        </main>
+    </body>
+</html>
