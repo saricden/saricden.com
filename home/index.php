2602ec7188d3833ad1f90bed34c7c64f3f9a4233
Kirk Morris <kirk@saricden.com>
2014-12-08 20:33:10 -0800

    Implements Google fonts and finishes style on side nav.

--- home/index.php
+++ home/index.php
@@ -2,22 +2,23 @@
 <html>
     <head>
         <title>Kirk M. // saricDen</title>
+        <link href='http://fonts.googleapis.com/css?family=Cabin+Sketch|Open+Sans:300' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" type="text/css" href="../_src/css/main.css">
     </head>
     <body>
         <nav>
             <h2><strong>saricDen</strong> by Kirk</h2>
             <ul>
-                <li class="active"><a href="../home">Home &raquo;</a></li>
+                <li><a href="../home">Home &raquo;</a></li>
                 <li><a href="../about">About me &raquo;</a></li>
-                <li><a href="../projects">Projects <strong>(16)</strong> &raquo;</a></li>
+                <li class="active"><a href="../projects">Projects <strong>(16)</strong> &raquo;</a></li>
                 <li><a href="../guestbook">Guestbook <strong>(69)</strong> &raquo;</a></li>
                 <li><a href="../contact">Contact &raquo;</a></li>
             </ul>
         </nav>
         <main>
             <h1>Kirk M. // saricDen</h1>
-            
+            <p>[Frontend / Serverside / Game] Developer</p>
         </main>
     </body>
 </html>
