1606efffe72ed52809379d67ed25d5bedb5ab7fb
Kirk Morris <kirk@saricden.com>
2014-12-11 15:33:12 -0800

    Splits home into reusable include files.

---
+++ _src/inc/footer.php
@@ -0,0 +1,5 @@
+        </main>
+        
+        <script type="text/javascript" src="../_src/js/main.js"></script>
+    </body>
+</html>
--- home/index.php
+++ home/index.php
@@ -1,28 +1,6 @@
-<!doctype html>
-<html>
-    <head>
-        <title>Kirk M. // saricDen</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-        <link href='http://fonts.googleapis.com/css?family=Cabin+Sketch|Open+Sans:300' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" type="text/css" href="../_src/css/main.css">
-    </head>
-    <body>
-        <div id="nav-btn"><span></span><span></span><span></span></div>
-        <nav id="sidebar">
-            <h2><strong>saricDen</strong> by Kirk</h2>
-            <ul>
-                <li class="active"><a href="../home">Home &raquo;</a></li>
-                <li><a href="../about">About me &raquo;</a></li>
-                <li><a href="../projects">Projects <strong>(16)</strong> &raquo;</a></li>
-                <li><a href="../guestbook">Guestbook <strong>(69)</strong> &raquo;</a></li>
-                <li><a href="../contact">Contact &raquo;</a></li>
-            </ul>
-        </nav>
-        <main id="content">
+<?php include('../_src/inc/header.php'); ?>
+
             <h1>Kirk M. // saricDen</h1>
             <p>[Frontend / Backend / Game] Developer</p>
-        </main>
-        
-        <script type="text/javascript" src="../_src/js/main.js"></script>
-    </body>
-</html>
+            
+<?php include('../_src/inc/footer.php'); ?>
