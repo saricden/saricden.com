c26a048f6e86e77f79c1a1d1f3f6b088957d0ba6
Kirk Morris <kirk@saricden.com>
2014-12-12 20:42:20 -0500

    Gets homepage layout working with dummy image (to be replaced with a pimpin sprite of yours truly).

--- home/index.php
+++ home/index.php
@@ -2,5 +2,7 @@
             <section>
                 <h1>Kirk M. // saricDen</h1>
                 <p>[Frontend / Backend / Game] Developer</p>
+
+                <img class="sprite-me" src="../_src/img/dummy-img.png" alt> 
             </section>
 <?php include('../_src/inc/footer.php'); ?>
