d14f77afc7aa449fb99e42db4d5e60922c035071
Kirk Morris <kirk@saricden.com>
2014-12-15 19:00:23 -0800

    Finishes up the homepage gurl.

--- home/index.php
+++ home/index.php
@@ -2,6 +2,7 @@
             <section>
                 <h1>Kirk M. // saricDen</h1>
                 <p>[Frontend / Backend / Game] Developer</p>
+                <a class="center-btn" href="../about">About Me &raquo;</a>
 
                 <img class="sprite-me" src="../_src/img/kirk-sprite.png" alt> 
             </section>
