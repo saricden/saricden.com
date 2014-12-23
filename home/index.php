5d0f57cc4261376202fa1adc1622b0da09dcbb04
Kirk Morris <kirk@saricden.com>
2014-12-22 17:50:15 -0800

    Restructures semantic tags a bit & adds content + dummy image to about.

--- home/index.php
+++ home/index.php
@@ -2,11 +2,13 @@
     require_once('../_src/php/core.php');
     printHeader(0);
 ?>
-            <section>
-                <h1>Kirk M. // saricDen</h1>
-                <p>[Frontend / Backend / Game] Developer</p>
-                <a class="center-btn" href="../about">About Me &raquo;</a>
+            <article>
+                <header>
+                    <h1>Kirk M. // saricDen</h1>
+                    <p>[Frontend / Backend / Game] Developer</p>
+                    <a class="center-btn" href="../about">About Me &raquo;</a>
+                </header>
 
                 <img class="sprite-me" src="../_src/img/kirk-sprite.png" alt> 
-            </section>
+            </article>
 <?php printFooter(); ?>
