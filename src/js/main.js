3d022d8eafbcc4fbb550ac48cbbbbaeb54e3001a
Kirk Morris <kirk@saricden.com>
2015-03-05 22:37:16 -0500

    Adds shitty contact page for now.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
+/*
+
+This is going to be refactored so hard.
+
+*/
+
 function e(id) {
   return document.getElementById(id);
 }
@@ -12,3 +18,19 @@ function openNavMenu(e) {
 };
 
 navMenuBtn.onclick = openNavMenu;
+
+if (e('sosneaky')) {
+  var falnfsa = new Array(0x6B, 'i', 0x72, 'k', 0x40, 'sar', 0x69, 'cde', 0x6E, 0x2E, 'c', 'o', 0x6D);
+  var jjnlngf = '';
+  for (var i in falnfsa) {
+    var c = falnfsa[i];
+    if (typeof c == 'string') {
+      jjnlngf += c;
+    }
+    else {
+      jjnlngf += String.fromCharCode(c);
+    }
+  }
+  e('sosneaky').href = "mailto:"+jjnlngf;
+  e('sosneaky').innerHTML = jjnlngf;
+}
