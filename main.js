28c085b7ba4649dccf4aaa768fd3a08695bc42f7
Kirk Morris <kirk@saricden.com>
2016-06-19 11:31:06 -0700

    Clears out everything...

--- main.js
+++
@@ -1,6 +0,0 @@
-// Set height of full panels
-var fullpanels = document.querySelectorAll(".fullheight");
-var screenHeight = window.innerHeight;
-for (i = 0; i < fullpanels.length; i++) {
-  fullpanels[i].style.height = screenHeight+"px";
-}
--- readme.md
+++
@@ -1,2 +0,0 @@
-<h1>Kirk's super cereal website about things</h1>
-Official repo for my personal site <a href="http://saricden.com/">saricden.com</a>, check it out gurl.
