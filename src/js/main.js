0fe1d926203aab0bed5d44c6fd3f8be333b10b07
Kirk Morris <kirk@saricden.com>
2017-03-05 11:40:52 -0800

    Adds JS for mobile side-menu.

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,15 @@
 var toggleMenu = function() {
-  var menuItems = document.querySelectorAll(".tabs a");
-  for (var i = 0; i < menuItems.length; i++) {
-    if (menuItems[i].className != "mobile-menu") {
-      var isOpen = (menuItems[i].style.display == "block");
-      if (isOpen) {
-        menuItems[i].style.display = "none";
-      }
-      else {
-        menuItems[i].style.display = "block";
-      }
-    }
+  document.querySelector(".side-column").classList.add("open");
+  document.querySelector(".overlay").classList.add("open");
+};
+
+var closeAll = function() {
+  var openOverlays = document.querySelectorAll(".open");
+  for (var i = 0; i < openOverlays.length; i++) {
+    openOverlays[i].classList.remove("open");
   }
 };
 
 document.querySelector(".mobile-menu").addEventListener("click", toggleMenu);
+document.querySelector(".mobile-close").addEventListener("click", closeAll);
+document.querySelector(".overlay").addEventListener("click", closeAll);
