8106e48969259b2c1f1d0c0ecf146385fc1472df
Kirk Morris <kirk@saricden.com>
2017-07-26 22:33:49 -0700

    Updates nav w/ sliding side menu.

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,11 @@
-var toggleMenu = function() {
-  document.querySelector(".side-column").classList.add("open");
-  document.querySelector(".overlay").classList.add("open");
+var nav = document.getElementById("navMenu");
+
+var openNav = function() {
+  nav.classList.add("open");
+};
+var closeNav = function() {
+  nav.classList.remove("open");
 };
 
-var closeAll = function() {
-  var openOverlays = document.querySelectorAll(".open");
-  for (var i = 0; i < openOverlays.length; i++) {
-    openOverlays[i].classList.remove("open");
-  }
-};
-
-document.querySelector(".mobile-menu").addEventListener("click", toggleMenu);
-document.querySelector(".mobile-close").addEventListener("click", closeAll);
-document.querySelector(".overlay").addEventListener("click", closeAll);
+document.getElementById("openNavBtn").addEventListener("click", openNav);
+document.getElementById("closeNavBtn").addEventListener("click", closeNav);
