24dc157b203c15bdcb567b6671a4a68465b3a4e8
Kirk Morris <kirk@saricden.com>
2017-07-29 10:55:50 -0700

    Adds overlay when nav menu comes out.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,15 @@
 var nav = document.getElementById("navMenu");
+var overlay = document.getElementById("overlay");
 
 var openNav = function() {
   nav.classList.add("open");
+  overlay.classList.add("open");
 };
 var closeNav = function() {
   nav.classList.remove("open");
+  overlay.classList.remove("open");
 };
 
 document.getElementById("openNavBtn").addEventListener("click", openNav);
 document.getElementById("closeNavBtn").addEventListener("click", closeNav);
+document.getElementById("overlay").addEventListener("click", closeNav);
