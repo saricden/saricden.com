489a1bb02a8e60cc6dc51fda8c69489e48312a82
Kirk Morris <worker.kirk@gmail.com>
2021-02-15 19:25:58 -0800

    Big overhaul part 1.

--- pages/_app.js
+++ pages/_app.js
@@ -1,49 +1,69 @@
 import '../styles/globals.css';
-import {useRouter} from 'next/router';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitch, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope, faMusic, faNewspaper, faWindowRestore} from '@fortawesome/free-solid-svg-icons';
+import {Fragment} from 'react';
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  const activeClass = [
-    (router.pathname === '/' ? 'active' : ''),
-    (router.pathname === '/open-haven' ? 'active' : ''),
-    (router.pathname === '/projects' ? 'active' : '')
-  ];
-
   return (
-    <div>
-      <nav className="top">
+    <Fragment>
+      <nav className="nav-primary">
         <Link href="/">
-          <a><img src="/me.svg" alt="Kirk M. (@saricden)" /></a>
+          <a className="nav-primary__logo">Kirk M. <span className="nav-primary__handle">@saricden</span></a>
         </Link>
+
+        <a className="nav-primary__social-link" href="https://github.com/saricden" target="_blank">
+          <FontAwesomeIcon className="nav-primary__social-icon" icon={faGithub} />
+        </a>
+
+        <a className="nav-primary__social-link" href="https://www.twitch.tv/saricden" target="_blank">
+          <FontAwesomeIcon className="nav-primary__social-icon" icon={faTwitch} />
+        </a>
+
+        <a className="nav-primary__social-link" href="https://twitter.com/saricden" target="_blank">
+          <FontAwesomeIcon className="nav-primary__social-icon" icon={faTwitter} />
+        </a>
       </nav>
-
       <Component {...pageProps} />
-
-      <nav className="bottom">
-        <Link href="/">
-          <a className={activeClass[0]}>
-            <img src="/tv.svg" alt="" />
-            <span>Streams</span>
+      <nav className="nav-footer">
+        <Link href="/blog">
+          <a className="nav-footer__link">
+            <FontAwesomeIcon className="nav-footer__icon" icon={faNewspaper} />
+            <label className="nav-footer__label">
+              Blog
+            </label>
           </a>
         </Link>
 
-        <Link href="/open-haven">
-          <a className={activeClass[1]}>
-            <img src="/open-haven.svg" alt="" />
-            <span>Open Haven</span>
+        <Link href="/music">
+          <a className="nav-footer__link">
+            <FontAwesomeIcon className="nav-footer__icon" icon={faMusic} />
+            <label className="nav-footer__label">
+              Music
+            </label>
           </a>
         </Link>
 
         <Link href="/projects">
-          <a className={activeClass[2]}>
-            <img src="/folder.svg" alt="" />
-            <span>Projects</span>
+          <a className="nav-footer__link">
+            <FontAwesomeIcon className="nav-footer__icon" icon={faWindowRestore} />
+            <label className="nav-footer__label">
+              Projects
+            </label>
+          </a>
+        </Link>
+
+        <Link href="/contact">
+          <a className="nav-footer__link">
+            <FontAwesomeIcon className="nav-footer__icon" icon={faEnvelope} />
+            <label className="nav-footer__label">
+              Contact
+            </label>
           </a>
         </Link>
       </nav>
-    </div>
+    </Fragment>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,197 +1,64 @@
 import Head from 'next/head';
 import {Component} from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faTwitch, faYoutube, faGithub, faStackOverflow, faTwitter, faInstagram} from '@fortawesome/free-brands-svg-icons';
 
-class Home extends Component {
-
+export default class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      untilNextStream: {
-        days: null,
-        hours: null,
-        minutes: null,
-        seconds: null
-      }
+      dh_yShift: 0
     };
 
-    this.canvasRef = null;
-    this.containerRef = null;
-
-    this.timer = null;
-
-    this.setCanvasRef = this.setCanvasRef.bind(this);
-    this.setContainerRef = this.setContainerRef.bind(this);
+    this.ticker = null;
+    this.rollHeading = this.rollHeading.bind(this);
   }
 
-  setCanvasRef(element) {
-    this.canvasRef = element;
-  }
-
-  setContainerRef(element) {
-    this.containerRef = element;
-  }
-
-  getNextStreamInterval() {
-    const now = DateTime.local();
-    console.log(now.ts);
-    const start = DateTime.local()
-  }
-
-  async componentDidMount() {
-    const dayIndex = 5; // Friday
-    const nextFriday = new Date();
-
-    const nextStreamUTC = new Date();
-    nextStreamUTC.setUTCDate(nextStreamUTC.getDate() + (dayIndex - nextFriday.getDay() + 7) % 7);
-    nextStreamUTC.setUTCHours(23, 0, 0, 0); // 11pm UTC = 4pm PDT
-
-    const now = (new Date().getTime() / 1000);
-    const streamTime = (nextStreamUTC.getTime() / 1000);
-    let seconds = streamTime - now;
-
-    this.timer = setInterval(() => {
-      const days        = Math.floor(seconds/24/60/60);
-      const hoursLeft   = Math.floor((seconds) - (days*86400));
-      const hours       = Math.floor(hoursLeft/3600);
-      const minutesLeft = Math.floor((hoursLeft) - (hours*3600));
-      const minutes     = Math.floor(minutesLeft/60);
-      const remainingSeconds = Math.floor(seconds % 60);
-
-      const untilNextStream = {
-        days,
-        hours,
-        minutes,
-        seconds: remainingSeconds
-      };
-
-      this.setState({ untilNextStream });
-
-      if (seconds === 0) {
-        clearInterval(countdownTimer);
-        console.log('DONE');
-      } else {
-        seconds--;
-      }
-    }, 1000);
-
-    
-
-    if (this.canvasRef && this.containerRef && typeof window !== 'undefined') {
-      const Phaser = (await import('phaser')).default;
-      const BootScene = (await import('../phaser_scenes/BootScene')).default;
-      const MainScene = (await import('../phaser_scenes/MainScene')).default;
-      const {Game} = Phaser;
-
-      const config = {
-        type: Phaser.WEB_GL,
-        scale: {
-          mode: Phaser.Scale.RESIZE,
-          parent: this.containerRef,
-          width: '100%',
-          height: '100%'
-        },
-        canvas: this.canvasRef,
-        physics: {
-          default: 'arcade',
-          arcade: {
-            gravity: { y: 400 },
-            debug: true
-          }
-        },
-        backgroundColor: '#88CCF3',
-        pixelArt: true,
-        scene: [
-          BootScene,
-          MainScene
-        ]
-      };
-
-      const game = new Game(config);
-    }
+  componentDidMount() {
+    this.ticker = setInterval(this.rollHeading, 1500);
   }
 
   componentWillUnmount() {
-    if (this.timer) {
-      clearInterval(this.timer);
-    }
+    clearInterval(this.ticker);
+  }
+
+  rollHeading() {
+    const min = 0;
+    const max = 4;
+    const yShift = 50; // look into a better way of doing this
+    let dh_yShift = 0;
+
+    dh_yShift = ((Math.floor(Math.random() * max) + min) * yShift);
+
+    this.setState({ dh_yShift });
   }
 
   render() {
-    const {untilNextStream} = this.state;
-    const {days, hours, minutes, seconds} = untilNextStream;
-
-    const counterVisible = (days !== null && hours !== null && minutes !== null && seconds !== null);
+    const {dh_yShift} = this.state;
 
     return (
-      <main>
-        <Head>
-          <title>Kirk M. (@saricden) - Game Dev Streamer</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+        <main className="content-body">
+          <Head>
+            <title>Create Next App</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
 
-        
-
-        <div className="canvas-con" ref={this.setContainerRef}>
-          <canvas ref={this.setCanvasRef}>Need JS</canvas>
-        </div>
-
-        <header>
-          <h1>Kirk M. (@saricden) is a game dev streamer on Twitch.tv</h1>
-          <p style={{
-            visibility: (counterVisible ? 'visible' : 'hidden')
-          }}>
-            <span>Live in</span>
-            <span><strong>{days}</strong>d</span>
-            <span><strong>{hours}</strong>h</span>
-            <span><strong>{minutes}</strong>m</span>
-            <span><strong>{seconds}</strong>s</span>
-          </p>
-          <a className="btn" href="https://twitch.tv/saricden" target="_blank">
-            <FontAwesomeIcon icon={faTwitch} />
-            Follow on Twitch
-          </a>
-          <a>Learn More</a>
-        </header>
-
-        <section>
-          <div className="about">
-            <h2>
-              Hi, my name is Kirk, and I like to make open source video games and stream about it on Twitch.
-            </h2>
-            <div className="social-links">
-              <a href="https://www.twitch.tv/saricden" target="_blank">
-                <FontAwesomeIcon icon={faTwitch} />
-              </a>
-              <a href="https://www.youtube.com/channel/UCKcZ5BgUNa40bEBt08LAfMg" target="_blank">
-                <FontAwesomeIcon icon={faYoutube} />
-              </a>
-              <a href="https://github.com/saricden" target="_blank">
-                <FontAwesomeIcon icon={faGithub} />
-              </a>
-              <a href="https://stackoverflow.com/users/1705378/saricden?tab=profile" target="_blank">
-                <FontAwesomeIcon icon={faStackOverflow} />
-              </a>
-              <a href="https://twitter.com/saricden" target="_blank">
-                <FontAwesomeIcon icon={faTwitter} />
-              </a>
-              <a href="https://www.instagram.com/saricden" target="_blank">
-                <FontAwesomeIcon icon={faInstagram} />
-              </a>
+          <h1 className="dynamic-heading">
+            Kirk M. is a(n)
+            <div className="dynamic-heading__roller-container">
+              <ul className="dynamic-heading__roller" style={{
+                transform: `translateY(-${dh_yShift}px)`
+              }}>
+                <li className="dynamic-heading__roll-item">Software Engineer</li>
+                <li className="dynamic-heading__roll-item">Indie Game Developer</li>
+                <li className="dynamic-heading__roll-item">Wannabe DJ</li>
+                <li className="dynamic-heading__roll-item">Tech Blogger</li>
+              </ul>
             </div>
-          </div>
+            in Victoria, BC
+          </h1>
 
-          <div className="videoflex">
-            <div className="videobox">
-              <iframe src="https://www.youtube.com/embed/ePMMP0naNLk" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-            </div>
-          </div>
-        </section>
-      </main>
+          <img className="avatar-big" src="/me.png" alt="Kirk M. (@saricden)" />
+        </main>
     );
   }
 }
-
-export default Home;
