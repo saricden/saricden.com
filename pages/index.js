475c648a351ac0881719c5c091cacde8267eae6b
Kirk Morris <worker.kirk@gmail.com>
2021-02-16 01:14:11 -0800

    Some updates... Blog coming soon.

--- pages/index.js
+++ pages/index.js
@@ -6,24 +6,38 @@ export default class Home extends Component {
     super(props);
 
     this.state = {
-      dh_yShift: 0
+      dh_yShift: 0,
+      screenHeight: 0
     };
 
     this.ticker = null;
     this.rollHeading = this.rollHeading.bind(this);
+    this.setScreenHeight = this.setScreenHeight.bind(this);
   }
 
   componentDidMount() {
     this.ticker = setInterval(this.rollHeading, 1500);
+    this.setScreenHeight();
+
+    window.addEventListener('resize', this.setScreenHeight);
   }
 
   componentWillUnmount() {
     clearInterval(this.ticker);
+    window.removeEventListener('resize', this.setScreenHeight);
+  }
+
+  setScreenHeight() {
+    if (window) {
+      this.setState({
+        screenHeight: window.innerHeight
+      });
+    }
   }
 
   rollHeading() {
     const min = 0;
-    const max = 4;
+    const max = 5;
     const yShift = 50; // look into a better way of doing this
     let dh_yShift = 0;
 
@@ -33,12 +47,14 @@ export default class Home extends Component {
   }
 
   render() {
-    const {dh_yShift} = this.state;
+    const {dh_yShift, screenHeight} = this.state;
 
     return (
-        <main className="content-body">
+        <main className="content-body" style={{
+          minHeight: `${screenHeight}px`
+        }}>
           <Head>
-            <title>Create Next App</title>
+            <title>Kirk M. (@saricden) - Software Engineer in Victoria, BC</title>
             <link rel="icon" href="/favicon.ico" />
           </Head>
 
@@ -51,6 +67,7 @@ export default class Home extends Component {
                 <li className="dynamic-heading__roll-item">Software Engineer</li>
                 <li className="dynamic-heading__roll-item">Indie Game Developer</li>
                 <li className="dynamic-heading__roll-item">Wannabe DJ</li>
+                <li className="dynamic-heading__roll-item">Code Mentor</li>
                 <li className="dynamic-heading__roll-item">Tech Blogger</li>
               </ul>
             </div>
